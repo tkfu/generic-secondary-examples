d4d2f3799377fd1eb4eee15b55e83ff217ce9372
Jon Oster <jon.oster@toradex.com>
2022-12-18 02:19:48 +0100

    Add config manager handler and config package generator
    Signed-off-by: Jon Oster <jon.oster@toradex.com>

---
+++ build-config-package.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import json
+import base64
+
+parser=argparse.ArgumentParser(description="Create configuration management packages for the Toradex Labs Simple Configuration Manager")
+parser.add_argument(
+	"--add", 
+	nargs=2, 
+	action="append",
+	metavar=("destination_path","config_file"),
+	help="Adds a file to the config management package. <destination_path> is the \
+	fully-qualified location on the device of the file that will be managed. \
+	<config_file> is the file you want to put there. May be specified multiple times."
+	)
+parser.add_argument(
+	"--delete", 
+	action="append", 
+	metavar="destination_path",
+	help="Adds a file to the configuration package that should be deleted. May be specified multiple times."
+	)
+parser.add_argument(
+	"output_file",
+	help="The name of the package to be written. This is the file you can upload to the Torizon platform."
+	)
+
+args = parser.parse_args()
+
+def build_config_package():
+	firmware_dict = {}
+	if args.add is not None:
+		for file in args.add:
+			device_path = file[0]
+			local_path = file[1]
+			firmware_dict[device_path] = {}
+			if os.path.exists(local_path):
+				firmware_dict[device_path]['content'] = base64.b64encode((open(local_path,"rb").read())).decode()
+				firmware_dict[device_path]['exists'] = True
+			else:
+				print("Error: local file " + local_path + "does not exist.")
+	if args.delete is not None:
+		for file in args.delete:
+			device_path = file
+			firmware_dict[device_path] = {}
+			firmware_dict[device_path]['exists'] = False
+	fw_string = dumps_json_canonical(firmware_dict)
+	with open(args.output_file,"w") as f:
+		f.write(fw_string)
+
+def dumps_json_canonical(mydict):
+	return json.dumps(mydict,separators=(',',':'),sort_keys=True,ensure_ascii=False,allow_nan=False)
+
+build_config_package()
---
+++ config-manager-handler.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+import argparse
+import hashlib
+import os
+import json
+import base64
+
+parser=argparse.ArgumentParser()
+parser.add_argument("action")
+parser.add_argument("-b", "--baseconfig")
+parser.add_argument("-o", "--outfile")
+
+
+# you can set your base config here, or pass it in with an argument
+baseconfig_string='''
+{
+    "/home/torizon/test.txt":
+    {
+        "exists": true,
+        "content": "ZXhhbXBsZSAxCg=="
+    }
+}
+'''
+baseconfig=json.loads(baseconfig_string)
+
+args = parser.parse_args()
+if args.baseconfig is not None:
+	with open(baseconfig,"r") as f:
+		baseconfig = json.load(f)
+action = args.action
+
+def get_firmware_info():
+	firmware_dict = {}
+	firmware_info = {}
+	for filepath in baseconfig.keys():
+		firmware_dict[filepath] = {}
+		if os.path.exists(filepath):
+			firmware_dict[filepath]['content'] = base64.b64encode((open(filepath,"rb").read())).decode()
+			firmware_dict[filepath]['exists'] = True
+		else:
+			firmware_dict[filepath]['exists'] = False
+	fw_string = dumps_json_canonical(firmware_dict)
+	firmware_info['sha256'] = hashlib.sha256(fw_string.encode('utf-8')).hexdigest()
+	firmware_info['length'] = len(fw_string)
+	firmware_info['status'] = 'ok'
+	firmware_info['message'] = 'Configmanager: Reported config for files: ' + ', '.join(baseconfig.keys())
+	return firmware_info
+
+def install():
+	install_report = {}
+	newfile = os.environ.get('SECONDARY_FIRMWARE_PATH')
+	oldfile = os.environ.get('SECONDARY_FIRMWARE_PATH_PREV')
+	with open(newfile,"r") as f:
+		configfile_string = f.read()
+		newconfig = json.loads(configfile_string)
+	# Check that the supplied file is canonicalized
+	if not dumps_json_canonical(newconfig) == configfile_string:
+		raise RuntimeError('Bad package supplied: package must be canonicalized JSON')
+	try:
+		apply_configurations(newconfig)
+	except Exception as ex:
+		try:
+			with open(oldfile,"r") as f:
+				oldconfig = json.load(f)
+			apply_configurations(oldconfig)
+		except Exception as rollback_ex:
+			try:
+				apply_configurations(baseconfig)
+			except Exception as baseconfig_ex:
+				install_report['status'] = 'failed'
+				install_report['message'] = 'Configmanager: failed to apply config with error: ' + repr(ex) \
+				  + '. Rollback to previous config also failed, with error: ' + repr(rollback_ex) \
+				  + '. Rollback to base config also failed, with error: ' + repr(rollback_ex)
+				return install_report
+			else:
+				install_report['status'] = 'failed'
+				install_report['message'] = 'Configmanager: failed to apply config with error: ' + repr(ex) \
+				  + '. Rollback to previous config also failed, with error: ' + repr(rollback_ex) \
+				  + ', but rollback to base config was successful.'
+				return install_report
+		else:
+			install_report['status'] = 'failed'
+			install_report['message'] = 'Configmanager: failed to apply config with error: ' + repr(ex) \
+			  + ', but rollback to previous config was successful.'
+			return install_report
+	else:
+		install_report['status'] = 'ok'
+		install_report['message'] = 'Configmanager: Applied config for files: ' + ', '.join(baseconfig.keys())
+		return install_report
+
+def apply_configurations(myconfig):
+	# check if list of files in the config file we want to apply exactly matches the list in the configmanager config
+	if not myconfig.keys() == baseconfig.keys():
+		raise RuntimeError('Mismatched file list: attempted to apply configs for files ' + ', '.join(myconfig.keys()) \
+			  + ', but this device requires configs for files ' + ', '.join(baseconfig.keys()))
+	for filepath in myconfig:
+		if myconfig[filepath]['exists']:
+			filecontent = base64.b64decode(myconfig[filepath]['content'])
+			with open(filepath,"wb") as f:
+				f.write(filecontent)
+		else:
+			if os.path.exists(filepath):
+				os.remove(filepath)
+
+
+def dumps_json_canonical(mydict):
+	return json.dumps(mydict,separators=(',',':'),sort_keys=True,ensure_ascii=False,allow_nan=False)
+
+def dump_current_firmware():
+	firmware_dict = {}
+	for filepath in baseconfig.keys():
+		firmware_dict[filepath] = {}
+		if os.path.exists(filepath):
+			firmware_dict[filepath]['content'] = base64.b64encode((open(filepath,"rb").read())).decode()
+			firmware_dict[filepath]['exists'] = True
+		else:
+			firmware_dict[filepath]['exists'] = False
+	fw_string = dumps_json_canonical(firmware_dict)
+	if args.outfile is not None:
+		with open(args.outfile,"w") as f:
+			f.write(fw_string)
+	else:
+		print(fw_string,end="")
+
+
+if action == 'get-firmware-info':
+	output = {}
+	try:
+		output = get_firmware_info()
+	except Exception as ex:
+		print('Could not get firmware info: ' + repr(ex))
+		quit(65)
+	else:
+		print(json.dumps(output))
+		quit()
+elif action == 'install':
+	output = {}
+	try:
+		output = install()
+	except Exception as ex:
+		print('Install action failure: ' + repr(ex))
+		quit(65)
+	else:
+		print(json.dumps(output))
+		quit()
+elif action == 'dump-current-config':
+	dump_current_firmware()
+
+
+
+else:
+	print("Unsupported action: " + action)
+	quit(65)
